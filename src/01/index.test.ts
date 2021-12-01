1e1d60a0a40cd7a3764f7bc026bece088edfa851
Marek Stasikowski <marek.stasikowski@globalrelay.net>
2021-12-01 15:13:10 +0000

    Report no increases for single depth

--- src/01/index.test.ts
+++ src/01/index.test.ts
@@ -2,11 +2,14 @@ import depthIncreaseCount from './index';
 import fc from 'fast-check';
 
 describe('01', () => {
-  it('has no increases for no depths', () => {
+  it('has no increases for depths count less than 2', () => {
     fc.assert(
-      fc.property(fc.array<number>(fc.nat(), { maxLength: 0 }), (depths) => {
-        expect(depthIncreaseCount(depths)).toBe(0);
-      })
+      fc.property(
+        fc.array(fc.nat(), { minLength: 0, maxLength: 1 }),
+        (depths) => {
+          expect(depthIncreaseCount(depths)).toBe(0);
+        }
+      )
     );
   });
 });
