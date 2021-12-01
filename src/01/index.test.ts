8214e4e8374eae399bff6efa7f904c482902b975
Marek Stasikowski <marek.stasikowski@globalrelay.net>
2021-12-01 15:08:00 +0000

    Employ fast-check for tests

--- src/01/index.test.ts
+++ src/01/index.test.ts
@@ -1,7 +1,12 @@
 import depthIncreaseCount from './index';
+import fc from 'fast-check';
 
 describe('01', () => {
   it('has no increases for no depths', () => {
-    expect(depthIncreaseCount([])).toBe(0);
+    fc.assert(
+      fc.property(fc.array<number>(fc.nat(), { maxLength: 0 }), (depths) => {
+        expect(depthIncreaseCount(depths)).toBe(0);
+      })
+    );
   });
 });
