6ccc7b4f89eaffe05bc8b94b2e1a84d8e17c20f1
Marek Stasikowski <marek.stasikowski@globalrelay.net>
2021-12-03 01:17:30 +0000

    Calculate multiple increases

--- src/01/index.test.ts
+++ src/01/index.test.ts
@@ -1,6 +1,24 @@
 import depthIncreaseCount from './index';
 import fc from 'fast-check';
 
+const withNIncreasing = (n: number) =>
+  fc.tuple(
+    fc
+      .array(fc.nat())
+      .filter(
+        (depths) =>
+          depths.length > n &&
+          depths.reduce(
+            (increasingCount, depth, i) =>
+              i > 0 && depth > depths[i - 1]
+                ? increasingCount + 1
+                : increasingCount,
+            0
+          ) === n
+      ),
+    fc.constant(n)
+  );
+
 describe('01', () => {
   it('has no increases for depths count less than 2', () => {
     fc.assert(
@@ -8,6 +26,16 @@ describe('01', () => {
         fc.array(fc.nat(), { minLength: 0, maxLength: 1 }),
         (depths) => {
           expect(depthIncreaseCount(depths)).toBe(0);
+        }
+      )
+    );
+  });
+  it('calculates number of increases', () => {
+    fc.assert(
+      fc.property(
+        fc.integer({ min: 3, max: 7 }).chain((n) => withNIncreasing(n)),
+        ([depths, increases]) => {
+          expect(depthIncreaseCount(depths)).toBe(increases);
         }
       )
     );
--- src/01/index.ts
+++ src/01/index.ts
@@ -1,3 +1,8 @@
-const main = (depths: number[]): number => 0;
+const countIncreasingDepths = (depths: number[]): number =>
+  depths.reduce(
+    (increasingCount, depth, i) =>
+      i > 0 && depth > depths[i - 1] ? increasingCount + 1 : increasingCount,
+    0
+  );
 
-export default main;
+export default countIncreasingDepths;
